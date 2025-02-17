2dc3696fd57dde742d8e3c6dd945b2599f951118
Rita Huang <yuhruhgrape@gmail.com>
2025-02-17 20:57:27 +0800

    add auth gem and db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_16_142557) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_17_123705) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,5 +24,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_16_142557) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
