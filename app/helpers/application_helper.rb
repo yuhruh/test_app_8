5ef1e7afc9fa03dc0e22bddf47d93353afe4b4a1
Rita Huang <yuhruhgrape@gmail.com>
2025-02-18 20:35:14 +0800

    add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80})
+    email_address = user.email.downcase
+    hash = Digest::SHA256.hexdigest(email_address)
+    size = options[:size]
+    params = URI.encode_www_form('d' => default, 's' => size)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?#{params}"
+    image_tag(gravatar_url, all: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
