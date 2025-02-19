603d3f3b0d3577e48f1f200228aa960aa376517a
Rita Huang <yuhruhgrape@gmail.com>
2025-02-19 21:12:25 +0800

    controller methods as helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
